916de6ec3ab7eb9d47af96c4a0d8e49bebed2184
Resül Çay <resulcayop@gmail.com>
2023-04-09 04:01:23 +0300

    color layout added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
-    private  var drawingView: DrawingView? = null
+    private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +20,18 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setBrushSize(4f)
 
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.linear_layout_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[2] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.palette_selected)
+        )
+
         val imageButtonBrush: ImageButton = findViewById(R.id.image_button_brush)
         imageButtonBrush.setOnClickListener {
             showBrushSizeDialog()
         }
     }
-
     private fun showBrushSizeDialog()
     {
         val brushDialog = Dialog(this)
